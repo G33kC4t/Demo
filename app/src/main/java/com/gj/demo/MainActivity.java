0983b06e310b202a253e9e7864f155bcd6d45e8b
guanjian <guanjian@mail.51vv.com>
2017-06-30 10:26:28 +0800

    提交ButterKnife

--- app/src/main/java/com/gj/demo/MainActivity.java
+++ app/src/main/java/com/gj/demo/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -11,13 +14,16 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    @BindView(R.id.sample_text)
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+        ButterKnife.bind(this);
         tv.setText(stringFromJNI());
     }
 
